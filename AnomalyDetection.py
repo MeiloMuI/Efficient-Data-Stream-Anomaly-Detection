f98b673f5fe7519e5da8593889c03138070e5d64
“MeiloMuI” <hxy244198079@outlook.com>
2024-10-24 20:36:38 +0100

    add ema detection

--- AnomalyDetection.py
+++ AnomalyDetection.py
@@ -4,7 +4,7 @@ from collections import deque
 
 # Parameters for data stream simulation
 stream_length = 700
-alpha = 0.1 # Smoothing factor for EMA
+alpha = 0.1 # Smoothing factor for EMA, means that the current value has a lower influence on the EMA calculation.
 threshold = 3   # Z-score threshold for anomaly detection
 window_size = 50    # Size of rolling window for statistics
 
@@ -12,7 +12,8 @@ window_size = 50    # Size of rolling window for statistics
 def generate_data_stream():
     stream = []
     for i in range(stream_length):
-        seasonal = 10 * math.sin(2 * math.pi * i / 50) # Seasonality, it creates a sinusoidal wave that repeats every 50 time steps, with an amplitude of 10.
+        # Seasonality, it creates a sinusoidal wave that repeats every 50 time steps, with an amplitude of 10.
+        seasonal = 10 * math.sin(2 * math.pi * i / 50)
         trend = 0.1 * i # Regular pattern
         noise = random.gauss(0, 2) # Random noise
         value = seasonal + trend + noise
@@ -23,11 +24,14 @@ def generate_data_stream():
         stream.append(value)
     return stream
 
-# Exponential Moving Average (EMA)
+# Exponential Moving Average (EMA). 
+# EMA can be used to track trends in cyclical fluctuations.
+# EMA helps identify long-term trends by smoothing short-term fluctuations. When sudden changes occur in the data stream, EMA can quickly capture such changes and reflect them in a timely manner.
 def calculate_ema(current_val, prev_ema, alpha):
     return alpha * current_val + (1 - alpha) * prev_ema
 
 # Z-score
+# Z-score can effectively identify outliers in new data environments, especially when concept drift occurs.
 def calculate_z_score(value, mean, std_dev):
     if std_dev == 0:
         return 0
@@ -53,6 +57,8 @@ def detect_anomalies(data_stream, window_size, alpha, threshold):
 
         # Get rolling mean and std_dev
         rolling_mean, rolling_std = calculate_mean_std(rolling_window)
+        # 2 times the rolling standard deviation
+        ema_threshold = 2 * rolling_std  
         # Update EMA
         ema = calculate_ema(value, ema, alpha)
         # Get Z-score for anomaly detection
@@ -61,7 +67,10 @@ def detect_anomalies(data_stream, window_size, alpha, threshold):
         new_anomalies = []
         # Flag as anomaly if Z-score exceeds threshold
         if abs(z_score) > threshold:
-            new_anomalies.append((index, value))
+            new_anomalies.append((index, value, "z-score anomaly"))
+        # Flag as anomaly if ema exceeds threshold
+        if abs(value - ema) > ema_threshold:
+            new_anomalies.append((index, value, "EMA anomaly"))
 
         yield index, value, ema, rolling_mean, rolling_std, new_anomalies
 
